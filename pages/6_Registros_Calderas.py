134e1062a568ee25530e1ab09ebcb0bea9a5c143
clsoporte2bsf <clsoporte2@cgcsf.mx>
2025-11-20 14:50:14 -0600

    Update 6_Registros_Calderas.py

--- pages/6_Registros_Calderas.py
+++ pages/6_Registros_Calderas.py
@@ -3,14 +3,8 @@ import pandas as pd
 from sqlalchemy import create_engine, text
 from datetime import date, datetime
 
-# --- CONEXIÓN ---
-DB_USER = "postgres"
-DB_PASSWORD = "1234"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "parametros_alberca"
-
-engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+# --- CONEXIÓN A LA BASE DE DATOS (usando secrets) ---
+engine = create_engine(st.secrets["DB_CONNECTION"])
 
 st.set_page_config(page_title="Registros de Calderas", page_icon="📋")
 st.header("📘 Hisorial de Registros de Calderas")
