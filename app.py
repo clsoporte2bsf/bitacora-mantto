a204cefbd84d75033f20fdd3bf362a4f3dbe861f
clsoporte2bsf <clsoporte2@cgcsf.mx>
2025-11-20 14:19:42 -0600

    Update app.py
    Se agrega titulo

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ import streamlit as st
 st.set_page_config(page_title="Bitácora de Albercas", page_icon="🏊", layout="centered")
 
 st.header("🏊 Bitácora de Parámetros de Albercas y Cuerpos de Agua")
+st.subheader("Selecciona el formulario del lado izquierdo")
 st.image("alberca-olimpica.png")
