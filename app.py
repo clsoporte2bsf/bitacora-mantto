bdfb4fafb1530b6b37d95b5fd9242dcf8f0d8ede
clsoporte2bsf <clsoporte2@cgcsf.mx>
2025-11-20 12:59:32 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# CONFIGURACIÓN DE LA PÁGINA
+
+st.set_page_config(page_title="Bitácora de Albercas", page_icon="🏊", layout="centered")
+
+st.header("🏊 Bitácora de Parámetros de Albercas y Cuerpos de Agua")
+st.image("alberca-olimpica.png")
